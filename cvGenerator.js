e7a7658f8f761a339dda1d85f736e3be77f691a7
Misunhwang <anabelly1004@gmail.com>
2022-09-10 12:38:45 +1200

    add education, certification, role function
    add education, certification, role function

--- cvGenerator.js
+++ cvGenerator.js
@@ -20,6 +20,10 @@ app.get('/', function(req, res, next) {
 	res.render('home', {title: 'Home'});
 });
 
+app.get('home', function(req, res, next) {
+	res.render('home', {title: 'Home'});
+});
+
 app.get('/personalinfo', function(req, res, next) {
 	var sql = `SELECT * FROM user WHERE userid="${userid}"`;
 	console.log(sql);
@@ -28,6 +32,18 @@ app.get('/personalinfo', function(req, res, next) {
 		console.log(result);
 		res.render('personalinfo', {title: 'View Personal Info', userInfo: result});
 	});
+});
+
+app.get('/addRole', function(req, res, next) {
+	res.render('addRole', {title: 'Add Role'});
+});
+
+app.get('/addCertification', function(req, res, next) {
+	res.render('addCertification', {title: 'Add Certification'});
+});
+
+app.get('/addEducation', function(req, res, next) {
+	res.render('addEducation', {title: 'Add Education'});
 });
 
 app.post('/personalinfo', function(req, res, next) {
@@ -49,6 +65,80 @@ app.post('/personalinfo', function(req, res, next) {
     console.log('record Updated');
 		res.redirect('/personalinfo');
   });
+});
+
+app.post('/addRole', function(req, res, next) {
+  var role = req.body.role;
+
+	console.log(role);
+
+	var sql = `SELECT * FROM role WHERE rolename="${role}"`;
+	console.log(sql);
+	db.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log(result);
+
+		if(result.length > 0) {
+			console.log(role + " role already exists");
+		} else {
+			sql = `INSERT INTO role (rolename) VALUES ("${role}")`;
+			console.log(sql);
+		  db.query(sql, function(err, result) {
+		    if (err) throw err;
+		    console.log('record inserted');
+				res.render('home');
+		  });
+		}
+	});
+});
+
+app.post('/addCertification', function(req, res, next) {
+	var userid = req.body.userid;
+	var certificationname = req.body.certification;
+  var organization = req.body.organization;
+  var issueYear = req.body.issueYear;
+	var issueMonth = req.body.issueMonth;
+	var description = req.body.description;
+
+	var sql = `INSERT INTO certification (userid, certificationname, organization, issueYear, issueMonth, description)
+						 VALUES ("${userid}", "${certificationname}", "${organization}", "${issueYear}", "${issueMonth}", "${description}")`;
+	console.log(sql);
+	db.query(sql, function(err, result) {
+		if (err) throw err;
+		console.log('record inserted');
+		res.render('home');
+	});
+});
+
+app.post('/addEducation', function(req, res, next) {
+	var userid = req.body.userid;
+	var schoolname = req.body.schoolname;
+	var country = req.body.country;
+  var coursename = req.body.coursename;
+	var degree = req.body.degree;
+  var startYear = req.body.startYear;
+	var startMonth = req.body.startMonth;
+	var notFinish = req.body.notFinish;
+	var special = req.body.special;
+	var sql = "";
+
+	if(notFinish == "on") {
+		sql = `INSERT INTO education (userid, schoolname, country, coursename, degree, startYear, startMonth, special)
+					 VALUES ("${userid}", "${schoolname}", "${country}", "${coursename}", "${degree}", "${startYear}", "${startMonth}", "${special}")`;
+	} else {
+		var endYear = req.body.endYear;
+		var endMonth = req.body.endMonth;
+		var grade = req.body.grade;
+
+		sql = `INSERT INTO education (userid, schoolname, country, coursename, degree, startYear, startMonth, special, endYear, endMonth, grade)
+					 VALUES ("${userid}", "${schoolname}", "${country}", "${coursename}", "${degree}", "${startYear}", "${startMonth}", "${special}", "${endYear}", "${endMonth}", "${grade}")`;
+	}
+	console.log(sql);
+	db.query(sql, function(err, result) {
+		if (err) throw err;
+		console.log('record inserted');
+		res.render('home');
+	});
 });
 
 app.listen(5500);
